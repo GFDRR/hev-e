eeafa986a6ba480d58d26d5a044309d9c9b52d5d
Cezary Statkiewicz <cezio@thelirium.net>
2018-02-26 11:27:29 +0100

    app skeleton, configuration

---
+++ setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#########################################################################
+#
+# Copyright (C) 2017 OSGeo
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#########################################################################
+
+import os
+from distutils.core import setup
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name="gfdrr_det",
+    version="0.1",
+    author="",
+    author_email="",
+    description="gfdrr_det, based on GeoNode",
+    long_description=(read('README.rst')),
+    # Full list of classifiers can be found at:
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+    license="BSD",
+    keywords="gfdrr_det geonode django",
+    url='https://github.com/geosolutions-it/gfdrr_det',
+    packages=['gfdrr_det',],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+       'geonode>=2.5',
+    ],
+)
