a542e8df0fdef579fb88f48f5cc86dae9746cb26
allyoucanmap <bovio.stefano@gmail.com>
2018-03-02 17:15:11 +0100

    Update layout

--- frontend/js/plugins/Map.jsx
+++ frontend/js/plugins/Map.jsx
@@ -13,12 +13,14 @@ const {connect} = require('react-redux');
 const {MapPlugin, reducers, epics} = require('../../MapStore2/web/client/plugins/Map');
 const {resizeMap} = require('../../MapStore2/web/client/actions/map');
 const ContainerDimensions = require('react-container-dimensions').default;
+const {getStyle} = require('../../MapStore2/web/client/components/map/openlayers/VectorStyle');
 
 class ResizableMapComponent extends React.Component {
 
     static propTypes = {
         width: PropTypes.number,
         height: PropTypes.number,
+        options: PropTypes.options,
         onResizeMap: PropTypes.function
     };
 
@@ -37,7 +39,57 @@ class ResizableMapComponent extends React.Component {
 
     render() {
         return (
-           <MapPlugin {...this.props}/>
+            <MapPlugin
+                options={{
+                    ...this.props.options,
+                    onClickEvent: event => {
+                        const featuresAtPixel = event.target && event.target.getFeaturesAtPixel && event.target.getFeaturesAtPixel(event.pixel);
+                        if (featuresAtPixel && featuresAtPixel.length > 0) {
+                            // const propertiesArray = featuresAtPixel.map(feature => feature.getProperties());
+                        }
+                    },
+                    onMoveEvent: event => {
+                        const featuresAtPixel = event.target && event.target.getFeaturesAtPixel && event.target.getFeaturesAtPixel(event.pixel);
+                        let hrefs = [];
+                        if (featuresAtPixel && featuresAtPixel.length > 0) {
+                            hrefs = featuresAtPixel.map(feature => {
+                                const properties = feature.getProperties();
+                                return properties.href;
+                            }).filter(href => href);
+                        }
+                        const layers = event.target && event.target.getLayers && event.target.getLayers();
+                        const layersArray = layers && layers.getArray && layers.getArray();
+                        if (layersArray && layersArray.length > 0) {
+                            layersArray.forEach(layer => {
+                                if (layer.type === 'VECTOR' && layer.get('msId') === 'datasets_layer' && layer.getSource) {
+                                    layer.getSource().forEachFeature(feature => {
+                                        const properties = feature.getProperties();
+                                        if (hrefs.indexOf(properties.href) !== -1) {
+                                            // selected style
+                                            feature.setStyle(getStyle({
+                                                style: {
+                                                    color: '#ff489e', // 0071bc blue
+                                                    fillColor: '#edf1f2ee',
+                                                    weight: 7
+                                                }
+                                            }));
+                                        } else {
+                                            // default style
+                                            feature.setStyle(getStyle({
+                                                style: {
+                                                    color: '#ff489e', // 0071bc blue
+                                                    fillColor: '#edf1f2AA',
+                                                    weight: 2
+                                                }
+                                            }));
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    }
+                }}
+                {...this.props}/>
         );
     }
 }
