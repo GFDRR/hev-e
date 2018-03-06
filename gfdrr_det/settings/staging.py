f15afe2611bb671ae134d05cdc36a492203ab048
afabiani <alessio.fabiani@geo-solutions.it>
2018-03-06 16:04:17 +0100

    - update settings files

--- gfdrr_det/settings/staging.py
+++ gfdrr_det/settings/staging.py
@@ -42,22 +42,22 @@ if EMAIL_ENABLE:
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': get_environment_variable("GFDRR_DET_DB_NAME", defaulr="gfdrr_det"),
-        'USER': get_environment_variable("GFDRR_DET_DB_USER", defaulr='geonode'),
-        'PASSWORD': get_environment_variable("GFDRR_DET_DB_PASSWORD", defaulr='geonode'),
-        'HOST': get_environment_variable("GFDRR_DET_DB_HOST", defaulr='localhost'),
-        'PORT': get_environment_variable("GFDRR_DET_DB_PORT", defaulr='5432'),
+        'NAME': get_environment_variable("GFDRR_DET_DB_NAME", default_value="gfdrr_det"),
+        'USER': get_environment_variable("GFDRR_DET_DB_USER", default_value='geonode'),
+        'PASSWORD': get_environment_variable("GFDRR_DET_DB_PASSWORD", default_value='geonode'),
+        'HOST': get_environment_variable("GFDRR_DET_DB_HOST", default_value='localhost'),
+        'PORT': get_environment_variable("GFDRR_DET_DB_PORT", default_value='5432'),
         'CONN_TOUT': 900,
     },
     # vector datastore for uploads
     'datastore': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         # 'ENGINE': '',  # Empty ENGINE name disables
-        'NAME': get_environment_variable("GFDRR_DET_DB_DATA_NAME", defaulr="gfdrr_det_data"),
-        'USER': get_environment_variable("GFDRR_DET_DB_USER", defaulr='geonode'),
-        'PASSWORD': get_environment_variable("GFDRR_DET_DB_PASSWORD", defaulr='geonode'),
-        'HOST': get_environment_variable("GFDRR_DET_DB_HOST", defaulr='localhost'),
-        'PORT': get_environment_variable("GFDRR_DET_DB_PORT", defaulr='5432'),
+        'NAME': get_environment_variable("GFDRR_DET_DB_DATA_NAME", default_value="gfdrr_det_data"),
+        'USER': get_environment_variable("GFDRR_DET_DB_USER", default_value='geonode'),
+        'PASSWORD': get_environment_variable("GFDRR_DET_DB_PASSWORD", default_value='geonode'),
+        'HOST': get_environment_variable("GFDRR_DET_DB_HOST", default_value='localhost'),
+        'PORT': get_environment_variable("GFDRR_DET_DB_PORT", default_value='5432'),
         'CONN_TOUT': 900,
     }
 }
