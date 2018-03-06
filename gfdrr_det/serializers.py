f6296f6602a1b3f6ac8a50a4f1fcb89936db3ac7
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2018-03-06 12:36:47 +0000

    Added copyright headers to new files

--- gfdrr_det/serializers.py
+++ gfdrr_det/serializers.py
@@ -1,3 +1,13 @@
+#########################################################################
+#
+# Copyright 2018, GeoSolutions Sas.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+#########################################################################
+
 """Django REST framework serializers for GFDRR-DET"""
 
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
--- gfdrr_det/views.py
+++ gfdrr_det/views.py
@@ -1,3 +1,13 @@
+#########################################################################
+#
+# Copyright 2018, GeoSolutions Sas.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+#########################################################################
+
 """views for GFDRR-DET"""
 
 from django.db.models import Q
